5b3e6cab78dd0e08f5fefb44c8bfc4ccbfea8554
Bob Bass <bob@bbass.co>
2020-03-12 17:46:33 -0400

    added basic functionality

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        image: './images/vmSocks-green.jpg',
+        inventory: 8,
+        onSale: true
     }
 })
